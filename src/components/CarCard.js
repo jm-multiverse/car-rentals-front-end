11d22cc7ec3fc10a82ddfa77b5a071d7f3179f18
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-08-28 16:25:59 -0700

    refactor car-card flex

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -12,27 +12,27 @@ export default function CarCard({
 
 
   return (
-    <Card className='p-4 car-card ' onClick={onViewCarClick}>
+    <Card className='p-4 car-card' onClick={onViewCarClick}>
       <div className='d-flex'>
         <div className="car-thumbnail-container me-5">
           <img src={getCarThumbnail((carId % 10))} alt={`${make} ${model}`}
             className='w-100 h-100 object-fit-contain'
           />
         </div>
-        <div className=''>
-          <h3 className='fw-normal'>{make} {model}</h3>
+        <div className='flex-grow-1'>
+          <div className='d-flex justify-content-between'>
+            <h3 className='fw-normal'>{make} {model}</h3>
+            <div>
+              <h5 className='fw-normal mb-0'>{currencyFormatter.format(pricePerDay)}</h5>
+              <div className='text-muted'>per day</div>
+            </div>
+          </div>
           <h5 className='fw-normal mb-1'>{carType}</h5>
           <div className=''>Seats: {capacity}</div>
           <div className=''>Color: {color}</div>
           <div className=''>Year: {manufacturingYear}</div>
           <div className=''>{transmission}</div>
           <div className=''>{fuelType}</div>
-        </div>
-        <div className="d-flex flex-column flex-grow-1 justify-content-between">
-          <div className='text-end no-wrap ms-auto mt-1'>
-            <h5 className='fw-normal mb-0'>{currencyFormatter.format(pricePerDay)}</h5>
-            <div className='text-muted'>per day</div>
-          </div>
           <div className='action-buttons-container d-flex gap-2 justify-content-end'>
             <Button variant='primary' className='mt-auto reserve-button'>Reserve</Button>
             <Button variant='outline-secondary' className='mt-auto'>Save</Button>
